d2cc65e5d40c08d422402024d9fc5a09aeb59646
zhengxin <xin.zheng@lolaage.com>
2021-04-13 20:17:14 +0800

    牛课网代码提交

---
+++ design/suanfa/src/main/java/牛牛的数列/Solution.java
@@ -0,0 +1,18 @@
+package 牛牛的数列;
+
+import java.util.*;
+
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param nums int一维数组
+     * @return int
+     */
+    public int maxSubArrayLength (int[] nums) {
+        // write code here
+        return -1;
+    }
+}
