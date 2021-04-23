27da6115e25fa13c41aa4677b7c626729016b692
zhengxin <xin.zheng@lolaage.com>
2021-04-23 18:07:59 +0800

    牛课提交

---
+++ design/suanfa/src/main/java/牛牛分蛋糕/Solution.java
@@ -0,0 +1,26 @@
+package 牛牛分蛋糕;
+
+import java.util.*;
+
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     * <p>
+     * 处理函数，返回在所有分法中，蛋糕数量最少的盘子中分到最多的蛋糕数量
+     *
+     * @param n int整型 n个盘子
+     * @param a int整型 a蛋糕数量
+     * @param b int整型 b蛋糕数量
+     * @return int整型
+     */
+    public int splitCake(int n, int a, int b) {
+        if (a + b == n) {
+            return 1;
+        }
+
+
+        return 0;
+
+    }
+}
